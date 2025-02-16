bf8c6e1976d3dbe78352b94c6985fb93584e7668
abel <abelferede916@gmail.com>
2025-02-17 00:54:40 +0300

    web view for books added

--- librarymanagemet/librarymanagemet/doctype/book/book.py
+++ librarymanagemet/librarymanagemet/doctype/book/book.py
@@ -2,10 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 
 
-class Book(Document):
+class Book(WebsiteGenerator):
 
  def before_save(self):
 		self.details=f'{self.title} {self.autohor or ""}'
