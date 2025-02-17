8df42e6dca2c94bb192db21e953df6d5f5eaebc0
abel <abelferede916@gmail.com>
2025-02-17 12:27:32 +0300

    exception handled for duplicated Isbn code

--- librarymanagemet/librarymanagemet/doctype/book/book.py
+++ librarymanagemet/librarymanagemet/doctype/book/book.py
@@ -1,13 +1,20 @@
 # Copyright (c) 2025, abel and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
-
-
+from frappe.model.document import Document
 class Book(WebsiteGenerator):
 
- def before_save(self):
-		self.details=f'{self.title} {self.autohor or ""}'
-
-
+    def before_validate(self):  # Fix: Added 'self' to refer to the current object
+        if self.isbn:
+            existing_book = frappe.db.exists("Book", {
+                "isbn": self.isbn,
+                "title": ["!=", self.title]  # Ensure it's not the same book being updated
+            })
+            if existing_book:
+                frappe.throw(
+                    title="Duplicate ISBN",
+                    msg=f"A book with ISBN {self.isbn} already exists.",
+                    exc=frappe.DuplicateEntryError  # Fix: Correct exception handling
+                )
